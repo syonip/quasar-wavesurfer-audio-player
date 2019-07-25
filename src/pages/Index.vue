be891f416e2fafb3a60cb680b1d8fc3c5d70b30c
Johnny <syonip@gmail.com>
2019-07-25 15:06:03 +0300

    adding wavesurfer

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,28 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
-  </q-page>
+  <div id="waveform"></div>
 </template>
 
-<style>
-</style>
-
 <script>
+import WaveSurfer from "wavesurfer.js";
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data: () => ({
+    wavesurfer: null,
+  }),
+  async mounted() {
+    if (!this.wavesurfer) this.createWaveSurfer();
+  },
+  methods: {
+    createWaveSurfer() {
+      this.wavesurfer = WaveSurfer.create({
+        container: "#waveform",
+        barWidth: 3
+      });
+      this.wavesurfer.load(
+        "https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3"
+      );
+    }
+  }
 }
 </script>
